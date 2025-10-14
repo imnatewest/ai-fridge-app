e3959bd52eac141f47c5affa0c0db3b2f5207163
imnatewest <nathanwest@usf.edu>
2025-10-14 13:33:22 -0400

    init: add SwiftUI iOS project

---
+++ AIFridge/AIFridge/AIFridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIFridgeApp.swift
+//  AIFridge
+//
+//  Created by Nathan West on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct AIFridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
